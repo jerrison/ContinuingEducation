69023a544fbd991e928dde2abf134546e8dcf3bb
Jerrison Li <jll09c@my.fsu.edu>
2018-03-21 15:11:29 -0700

    finished optional graded problems

--- python/edx/introduction_to_computer_science_and_programming_using_python/week1/2_core_elements_of_programs/problem_set_1/problem1.py
+++
@@ -1,6 +0,0 @@
-vowels = ("a", "e", "i", "o", "u")
-count = 0
-for element in s:
-    if element in vowels:
-        count += 1
-print(count)
---
+++ web_programming/jQuery/beginning_jQuery_book/1/lesson_walkthrough.js
@@ -0,0 +1,20 @@
+/* Arrays */
+
+var classMates = ["Jack",
+    "Jamie",
+    "Rich",
+    "Will"
+];
+
+classMates[1]; //
+
+// access last element of array without knowing size of array
+classMates[classMates.length - 1]; // "Will"
+
+
+
+
+
+
+
+
