d71292c288794ea73b98285639a5cc469b04b96f
Jerrison Li <jll09c@my.fsu.edu>
2018-04-05 17:08:42 -0700

    format and trim whitespace

--- python/edx_introduction_to_computer_science_and_programming_using_python/problem_set_3/ps3_hangman.py
+++ python/edx_introduction_to_computer_science_and_programming_using_python/problem_set_3/ps3_hangman.py
@@ -92,28 +92,29 @@ def getAvailableLetters(lettersGuessed):
             s = s.replace(letter, "")
     return s
 
+
 def hangman(secretWord):
     '''
     secretWord: string, the secret word to guess.
 
     Starts up an interactive game of Hangman.
 
-    * At the start of the game, let the user know how many 
+    * At the start of the game, let the user know how many
       letters the secretWord contains.
 
     * Ask the user to supply one guess (i.e. letter) per round.
 
-    * The user should receive feedback immediately after each guess 
+    * The user should receive feedback immediately after each guess
       about whether their guess appears in the computers word.
 
-    * After each round, you should also display to the user the 
-      partially guessed word so far, as well as letters that the 
+    * After each round, you should also display to the user the
+      partially guessed word so far, as well as letters that the
       user has not yet guessed.
 
     Follows the other limitations detailed in the problem write-up.
     '''
     # FILL IN YOUR CODE HERE...
-    
+
 
 # When you've completed your hangman function, uncomment these two lines
 # and run this file to test! (hint: you might want to pick your own
